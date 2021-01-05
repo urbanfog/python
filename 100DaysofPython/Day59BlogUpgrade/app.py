11a82a5edff955e28d49f018a393464d10318b37
James Smith <theworld969@hotmail.com>
2021-01-04 18:51:17 -0700

    Day 60, updated blog to create working contact form.

--- 100DaysofPython/Day59BlogUpgrade/app.py
+++ 100DaysofPython/Day59BlogUpgrade/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
+
+# Load ENV variables
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv(), override=True)
+
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.GETENV("MY_PASS")
 
 
 class Post:
@@ -29,6 +37,16 @@ for p in all_data:
     all_blog_posts.append(post)
 
 
+def send_email(message, send_to):
+    msg = f"Subject: New Request!\n\n{message}".encode(encoding="utf8")
+    with smtplib.SMTP("smtp.live.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=send_to,
+                            msg=msg)
+
+
 @app.route('/')
 def home_page():
     return render_template("index.html", all_blog_posts=all_blog_posts)
@@ -39,9 +57,26 @@ def about_page():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact_page():
-    return render_template("contact.html")
+    h1 = None
+    if request.method == 'GET':
+        h1 = 'Contact Me'
+    if request.method == 'POST':
+        contact_form_data()
+        h1 = 'Successfully submitted form'
+        message = f'''
+            Name: {request.form["name"]}
+            Email: {request.form["email"]}
+            Phone: {request.form["phone_number"]}
+            Message: {request.form["message"]}
+            '''
+        send_email(send_to="jamescsmith@live.com", message=message)
+    return render_template("contact.html", h1=h1)
+
+
+def contact_form_data():
+    print(request.form['name'])
 
 
 @app.route('/post/<int:id>')
