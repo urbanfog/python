87b990441f318a98f46192723daeebcb7273fc08
James Smith <theworld969@hotmail.com>
2021-01-11 20:09:16 -0700

    Day 68 Finished

--- 100DaysofPython/Day68Auth/main.py
+++ 100DaysofPython/Day68Auth/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_login import UserMixin, login_manager, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
 
@@ -9,14 +9,21 @@ app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-##CREATE TABLE IN DB
+# CREATE TABLE IN DB
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-#Line below only required once, when creating DB. 
+
+
+# db.drop_all()
+# # Line below only required once, when creating DB.
 # db.create_all()
 
 
@@ -25,19 +32,48 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        password = request.form.get('password')
+        salted_password = generate_password_hash(
+            password, method='pbkdf2:sha256', salt_length=8)
+        new_user = User(email=request.form.get('email'),
+                        password=salted_password,
+                        name=request.form.get('name'),
+                        )
+        if not User.query.filter_by(email='email').first():
+            flash("Email address already exists.")
+            return redirect(url_for('login'))
+        else:
+            db.session.add(new_user)
+            db.session.commit()
+            return render_template('secrets.html', name=new_user.name, logged_in=current_user.is_authenticated)
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        print(user)
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('secrets'))
+        else:
+            flash('Incorrect password entered.')
+            return render_template("login.html", logged_in=current_user.is_authenticated)
+    else:
+        print('no post request')
+        return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=current_user.name, logged_in=current_user.is_authenticated)
 
 
 @app.route('/logout')
@@ -46,8 +82,15 @@ def logout():
 
 
 @app.route('/download')
+@login_required
 def download():
-    pass
+    return send_from_directory(
+        '/Users/smith/python/100DaysofPython/Day68Auth/static/files', 'cheat_sheet.pdf')
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 if __name__ == "__main__":
